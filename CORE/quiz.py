89854bdabc15467192fa1410dd20e5794ff539c7
Amine Mustapha <mustaphaamine54@gmail.com>
2026-07-18 12:21:31 +0900

    refactor: redesigned guess_meaning() using dict frequency counts, improved summary and logic flow

--- CORE/quiz.py
+++ CORE/quiz.py
@@ -11,27 +11,25 @@ class Quiz :
     
     def guess_meaning(self) :
         count = 0
-        duplicate = []
         interval = min(len(self.db), 15)
-        deplicate_counter = 0
+        duplicates = {}
+        showns = 0
 
-        while count < interval : #< prevent runnig 16 times
+        while count < interval : #< prevent runnig 16 times start at 0
 
             w = random.choice(self.db)
+            showns += 1
             
             print(f"what is the meaning of the following word {w["word"]}")
 
             answer = input("Enter your answer").strip()
 
             if answer == w["meaning"] :
-                if w["word"] not in duplicate :
-                    duplicate.append(w["word"])
-                    print("your answer is correct")
-                    count += 1
-                    
-                else :
-                    deplicate_counter += 1
+                if w["word"] not in duplicates :
 
+                    duplicates[w["word"]] = 0
+                duplicates[w["word"]] += 1
+                
             else :
                 print("your answer is incorrect")
                 print(f"the correct meaning : {w['meaning']}")
@@ -39,7 +37,13 @@ class Quiz :
                 print(f"Example : {w.get('example', 'use update tool to add example for better understanding !')}")
                 count -= 1
 
-        print(f"{interval} has been reviwed !, number of word shown more than 1 time {deplicate_counter}")
+        for w in duplicates.keys() :
+            if duplicates[w] == 1 :
+                del duplicates[w]
+        
+        only_duplicates = sum(count for count in duplicates.values()) - len(duplicates)
+
+        print(f"{showns} has been reviwed in total!, number of word shown more than 1 time {only_duplicates}")
         return
 
 
